374c4670b10166d8f8b7abb6ed96f58bcf3041a3
Kristoffer Sahlin <krsahlin@gmail.com>
2021-12-24 16:47:06 +0100

    +

--- scripts/get_time_alignment.py
+++ scripts/get_time_alignment.py
@@ -11,9 +11,8 @@ def parse_gnu_time(stderr_file):
     index_time_strobemap = False
 
     for l in lines:
-        usertime_match =  re.search('User time \(seconds\): [\d.]+', l)
-        wct_match = re.search('Elapsed \(wall clock\) time \(h:mm:ss or m:ss\): [\d.:]+', l) 
-        mem_match = re.search('Maximum resident set size \(kbytes\): [\d.:]+', l) 
+        wct_match = re.search('[\d]+ real', l) 
+        mem_match = re.search('[\d]+ maximum resident set size', l) 
 
         # minimap2
         index_time_mm2_match = re.search('\[M::main::[\d.:]+\*[\d.:]+\] loaded/built the index for', l) 
@@ -25,12 +24,10 @@ def parse_gnu_time(stderr_file):
         # accelalign
         index_time_accelalign_match = re.search('Setup reference in [\d.:]+ secs', l) 
 
-        if usertime_match:
-            usertime = float(usertime_match.group().split(':')[1].strip())
         if wct_match:
-            wallclocktime = wct_match.group().split()[7]
+            wallclocktime = wct_match.group().split()[0]
         if mem_match:
-            mem_tmp = int(mem_match.group().split()[5])
+            mem_tmp = int(mem_match.group().split()[0])
             memory_gb = mem_tmp / 1000000.0 
 
         if index_time_mm2_match:
@@ -47,31 +44,42 @@ def parse_gnu_time(stderr_file):
             final_time = prefix_cut.split(' secs')[0]
             index_time_aa = float(final_time.strip())
 
-    vals = list(map(lambda x: float(x), wallclocktime.split(":") ))
-    if len(vals) == 3:
-        h,m,s = vals
-        tot_wallclock_secs = h*3600.0 + m*60.0 + s
-        if index_time_mm2:
-            tot_wallclock_secs = tot_wallclock_secs - index_time_mm2
-        elif index_time_aa:
-            tot_wallclock_secs = tot_wallclock_secs - index_time_aa
-        elif index_time_strobemap:
-            tot_wallclock_secs = tot_wallclock_secs - index_time_strobemap
+    if index_time_mm2:
+        tot_wallclock_secs = tot_wallclock_secs - index_time_mm2
+    elif index_time_aa:
+        tot_wallclock_secs = tot_wallclock_secs - index_time_aa
+    elif index_time_strobemap:
+        tot_wallclock_secs = tot_wallclock_secs - index_time_strobemap
 
-    elif len(vals) == 2:
-        m,s = vals
-        tot_wallclock_secs = m*60.0 + s
+    return tot_wallclock_secs, memory_gb
+    
+    # vals = list(map(lambda x: float(x), wallclocktime.split(".") ))
+    # if len(vals) == 3:
+    #     h,m,s = vals
+    #     tot_wallclock_secs = h*3600.0 + m*60.0 + s
+    #     if index_time_mm2:
+    #         tot_wallclock_secs = tot_wallclock_secs - index_time_mm2
+    #     elif index_time_aa:
+    #         tot_wallclock_secs = tot_wallclock_secs - index_time_aa
+    #     elif index_time_strobemap:
+    #         tot_wallclock_secs = tot_wallclock_secs - index_time_strobemap
 
-        if index_time_mm2:
-            tot_wallclock_secs = tot_wallclock_secs - index_time_mm2
-        elif index_time_strobemap:
-            tot_wallclock_secs = tot_wallclock_secs - index_time_strobemap
+    # elif len(vals) == 2:
+    #     s, = vals
+    #     tot_wallclock_secs = m*60.0 + s
 
-    return usertime, tot_wallclock_secs, memory_gb
+    #     if index_time_mm2:
+    #         tot_wallclock_secs = tot_wallclock_secs - index_time_mm2
+    #     elif index_time_strobemap:
+    #         tot_wallclock_secs = tot_wallclock_secs - index_time_strobemap
 
+        # return vals, memory_gb
+
+    # else:
+    #     return "BUG","BUG"
 
 def main(args):
-    usertime, tot_wallclock_secs, memory_gb = parse_gnu_time(args.infile)
+    tot_wallclock_secs, memory_gb = parse_gnu_time(args.infile)
 
 
     print("{0},{1}".format(tot_wallclock_secs, memory_gb))
