27ee6edceac2c9666cb99ac74412da4151a53563
Kristoffer Sahlin <krsahlin@gmail.com>
2021-12-30 12:52:22 +0100

    +

--- evaluation_PE/plots.py
+++ evaluation_PE/plots.py
@@ -15,20 +15,20 @@ import pandas as pd
 from matplotlib import pyplot
 
 
-def plot_accuracy(input_csv, outfolder):
+def plot_accuracy(input_csv, outfolder, palette):
     matplotlib.rcParams.update({'font.size': 18})
     sns.set(font_scale=1.9)
     sns.set_style("whitegrid")
     indata = pd.read_csv(input_csv)
-    # "minimap2", "bwa_mem", "strobealign",'accelalign', "bowtie2", "strobealign_map", "minimap2_map", "accelalign_map"
+    # "minimap2",'accelalign', "bowtie2", "strobealign_map", "minimap2_map", "accelalign_map"
     # dashes = {"bwa_mem" : "", "bowtie2" : "",
     #             "minimap2" : "", "minimap2_map" : (5,5),
     #             "strobealign" : "", "strobealign_map" : (5,5),
     #             "accelalign" : "", "accelalign_map" : (5,5)}
     # print(indata)
     g = sns.relplot(data=indata, x="read_length", y="accuracy", hue="tool", style="type", kind="line", #dashes = dashes,
-        col="dataset", hue_order = ["minimap2", "bwa_mem", "strobealign",'accelalign', "bowtie2", "urmap", "snap"], # hue="datastructure", style="datastructure",
-        col_wrap=3, col_order=["SIM1", "SIM2", "SIM3"])
+        col="dataset", hue_order = ["minimap2",'accelalign', "bowtie2", "urmap", "snap", "bwa_mem", "strobealign"], # hue="datastructure", style="datastructure",
+        col_wrap=3, col_order=["SIM1", "SIM2", "SIM3"], palette=palette)
     # ax = sns.lineplot(data=indata, x="k", y="unique", hue="datastructure", style="chr", palette = sns.color_palette()[:7])
     # axes = g.axes
     g.set_axis_labels("Read length", "Accuracy")
@@ -37,14 +37,14 @@ def plot_accuracy(input_csv, outfolder):
     # ax.set_xlabel("k")
     # axes.set_xticks([18,24,30,36] )
     # ax.set_ylim((75, 100))
-    g.set(ylim=(94, 100), xticks=[100,150,200,250,300])
+    g.set(ylim=(96, 99), xticks=[100,150,200,250,300])
     # g.set(ylim=(95, 100))
     # ax.set_xticks([18,24,30,36])
     plt.savefig(os.path.join(outfolder, "accuracy_plot.eps"))
     plt.savefig(os.path.join(outfolder, "accuracy_plot.pdf"))
     plt.close()
 
-def plot_percentage_aligned(input_csv, outfolder):
+def plot_percentage_aligned(input_csv, outfolder, palette):
     matplotlib.rcParams.update({'font.size': 18})
     sns.set(font_scale=1.9)
     sns.set_style("whitegrid")
@@ -63,8 +63,8 @@ def plot_percentage_aligned(input_csv, outfolder):
     # print(indata)
     g = sns.relplot(
         data=indata, x="read_length", y="aligned", hue="tool", style="type",
-        col="dataset", kind="line",  hue_order = ["minimap2", "bwa_mem", "strobealign",'accelalign', "bowtie2", "urmap", "snap"], #dashes = dashes, hue="datastructure", style="datastructure",
-        col_wrap=3, col_order=["SIM1", "SIM2", "SIM3"])
+        col="dataset", kind="line",  hue_order = ["minimap2",'accelalign', "bowtie2", "urmap", "snap", "bwa_mem", "strobealign"], #dashes = dashes, hue="datastructure", style="datastructure",
+        col_wrap=3, col_order=["SIM1", "SIM2", "SIM3"], palette=palette)
     # ax = sns.lineplot(data=indata, x="k", y="unique", hue="datastructure", style="chr", palette = sns.color_palette()[:7])
     # axes = g.axes
     g.set_axis_labels("Read length", "Percentage aligned")
@@ -80,7 +80,7 @@ def plot_percentage_aligned(input_csv, outfolder):
     plt.savefig(os.path.join(outfolder, "percentage_aligned_plot.pdf"))
     plt.close()
 
-def plot_overaligned(input_csv, outfolder):
+def plot_overaligned(input_csv, outfolder, palette):
     matplotlib.rcParams.update({'font.size': 18})
     sns.set(font_scale=1.9)
     sns.set_style("whitegrid")
@@ -99,8 +99,8 @@ def plot_overaligned(input_csv, outfolder):
     # print(indata)
     g = sns.relplot(
         data=indata, x="read_length", y="overaligned", hue="tool", style="type",
-        col="dataset", kind="line", hue_order = ["minimap2", "bwa_mem", "strobealign",'accelalign', "bowtie2", "urmap", "snap"],  #dashes = dashes, hue="datastructure", style="datastructure",
-        col_wrap=3, col_order=["SIM1", "SIM2", "SIM3"])
+        col="dataset", kind="line", hue_order = ["minimap2",'accelalign', "bowtie2", "urmap", "snap", "bwa_mem", "strobealign"],  #dashes = dashes, hue="datastructure", style="datastructure",
+        col_wrap=3, col_order=["SIM1", "SIM2", "SIM3"], palette=palette)
     # ax = sns.lineplot(data=indata, x="k", y="unique", hue="datastructure", style="chr", palette = sns.color_palette()[:7])
     # axes = g.axes
     g.set_axis_labels("Read length", "Overaligned")
@@ -117,7 +117,7 @@ def plot_overaligned(input_csv, outfolder):
     plt.close()
 
 
-def plot_memory_usage(input_csv, outfolder):
+def plot_memory_usage(input_csv, outfolder, palette):
     matplotlib.rcParams.update({'font.size': 18})
     sns.set(font_scale=1.9)
     # tool,dataset,read_length,time,memory
@@ -127,8 +127,8 @@ def plot_memory_usage(input_csv, outfolder):
 
     g = sns.relplot(
         data=indata, x="read_length", y="memory", hue="tool", style="type",
-        col="dataset", kind="line", hue_order = ["minimap2", "bwa_mem", "strobealign",'accelalign', "bowtie2", "urmap", "snap"],  #dashes = dashes, hue="datastructure", style="datastructure",
-        col_wrap=3, col_order=["SIM1", "SIM2", "SIM3"])
+        col="dataset", kind="line", hue_order = ["minimap2",'accelalign', "bowtie2", "urmap", "snap", "bwa_mem", "strobealign"],  #dashes = dashes, hue="datastructure", style="datastructure",
+        col_wrap=3, col_order=["SIM1", "SIM2", "SIM3"], palette=palette)
     # ax = sns.lineplot(data=indata, x="k", y="unique", hue="datastructure", style="chr", palette = sns.color_palette()[:7])
     # axes = g.axes
     g.set_axis_labels("Read length", "Memory usage (Gb)")
@@ -144,7 +144,7 @@ def plot_memory_usage(input_csv, outfolder):
     plt.savefig(os.path.join(outfolder, "memory_plot.pdf"))
     plt.close()
 
-def plot_runtime(input_csv, outfolder):
+def plot_runtime(input_csv, outfolder, palette):
     matplotlib.rcParams.update({'font.size': 18})
     sns.set(font_scale=1.4)
     # tool,dataset,read_length,time,memory
@@ -153,8 +153,8 @@ def plot_runtime(input_csv, outfolder):
     indata = pd.read_csv(input_csv)
     g = sns.relplot(
         data=indata, x="read_length", y="time", hue="tool", style="type",
-        col="dataset", kind="line", hue_order = ["minimap2", "bwa_mem", "strobealign",'accelalign', "bowtie2", "urmap", "snap"],  #dashes = dashes, hue="datastructure", style="datastructure",
-        col_wrap=3, col_order=["SIM1", "SIM2", "SIM3"])
+        col="dataset", kind="line", hue_order = ["minimap2",'accelalign', "bowtie2", "urmap", "snap", "bwa_mem", "strobealign"],  #dashes = dashes, hue="datastructure", style="datastructure",
+        col_wrap=3, col_order=["SIM1", "SIM2", "SIM3"], palette=palette)
     # ax = sns.lineplot(data=indata, x="k", y="unique", hue="datastructure", style="chr", palette = sns.color_palette()[:7])
     # axes = g.axes
     g.set_axis_labels("Read length", "Time (sec)")
@@ -204,14 +204,23 @@ def add_column(infile):
 
 def main(args):
     sns.set_style("whitegrid")
+    palette = {
+    'minimap2': 'tab:blue',
+    'strobealign': 'tab:green',
+    'bwa_mem': 'tab:orange',
+    'accelalign': 'tab:red',
+    'bowtie2' : 'tab:purple',
+    'urmap' : 'tab:grey',
+    'snap' : 'pink'
+    }
     accuracy_csv = add_column(args.accuracy_csv)
     runtime_mem_csv = add_column(args.runtime_mem_csv)
 
-    plot_accuracy(accuracy_csv, args.outfolder)
-    plot_percentage_aligned(accuracy_csv, args.outfolder)
-    plot_overaligned(accuracy_csv, args.outfolder)
-    plot_runtime(runtime_mem_csv, args.outfolder)
-    plot_memory_usage(runtime_mem_csv, args.outfolder)
+    plot_accuracy(accuracy_csv, args.outfolder, palette)
+    plot_percentage_aligned(accuracy_csv, args.outfolder, palette)
+    plot_overaligned(accuracy_csv, args.outfolder, palette)
+    plot_runtime(runtime_mem_csv, args.outfolder, palette)
+    plot_memory_usage(runtime_mem_csv, args.outfolder, palette)
 
 
 if __name__ == '__main__':
--- scripts/mini_analysis.sh
+++ scripts/mini_analysis.sh
@@ -56,58 +56,65 @@ mkdir -p $outroot
 
 echo -n  "tool","ref","%-aligned","accuracy,time(sec),Mem(MB)"$'\n'
 
-# for read_lengh in 100 150 #100 #150 #200 250 300 
+# for read_lengh in 100 150 200 #100 #150 #200 250 300 
 # do 
-#     for chr_id in hg38_chr21 hg38_chr1 hg38_chr15 hg38_chr18 hg38_chrX  #hg38_chr1_2 hg38_chr6_15_18_X_Y # hg38_chr1 hg38_chr15 hg38_chr18 hg38_chr21 hg38_chrX #  # hg38_chr1_2 hg38_chr6_15_18_X_Y  #
+#     for chr_id in hg38_chrY # hg38_chr21 hg38_chr1 hg38_chr15 hg38_chr18 hg38_chrX  #hg38_chr1_2 hg38_chr6_15_18_X_Y # hg38_chr1 hg38_chr15 hg38_chr18 hg38_chr21 hg38_chrX #  # hg38_chr1_2 hg38_chr6_15_18_X_Y  #
 #     do
-#         if [[ ! -f $outroot/$chr_id/$read_lengh.sam ]]
-#         then
-#             echo "SIMULATING READS"
-#             mkdir -p $outroot/$chr_id/
-#             mason_simulator -ir $refs/$chr_id.fa -n 200000 --illumina-read-length $read_lengh --fragment-mean-size 3000 -o $outroot/$chr_id/$read_lengh.L.fq -or $outroot/$chr_id/$read_lengh.R.fq -oa $outroot/$chr_id/$read_lengh.sam
-#         fi
+#         # if [[ ! -f $outroot/$chr_id/$read_lengh.sam ]]
+#         # then
+#         #     echo "SIMULATING VARIANTS"
+#         #     mkdir -p $outroot/$chr_id/
+#         #     mason_variator --sv-indel-rate 0.000005 --snp-rate 0.001 --small-indel-rate 0.0001 --max-small-indel-size 50   -ir $refs/$chr_id.fa -ov $refs/$chr_id.vcf &> /dev/null
+#         #     echo "SIMULATING READS"
+#         #     mason_simulator -ir $refs/$chr_id.fa -iv $refs/$chr_id.vcf -n 200000 --illumina-read-length $read_lengh --fragment-mean-size 300 -o $outroot/$chr_id/$read_lengh.L.fq -or $outroot/$chr_id/$read_lengh.R.fq -oa $outroot/$chr_id/$read_lengh.sam
+#         # fi
 
-#         # minimap2 stats
-#         /usr/bin/time -l minimap2 -t 1 --eqx -ax sr $refs/$chr_id.fa $outroot/$chr_id/$read_lengh.L.fq $outroot/$chr_id/$read_lengh.R.fq 1> $outroot/$chr_id/$read_lengh.minimap2.sam 2>  $outroot/$chr_id/$read_lengh.minimap2.stderr
-#         # /usr/bin/time -l minimap2 -t 1 --eqx -ax map-hifi $refs/$chr_id.fa $outroot/$chr_id/$read_lengh.L.fq $outroot/$chr_id/$read_lengh.R.fq 1> $outroot/$chr_id/$read_lengh.minimap2.sam 2>  $outroot/$chr_id/$read_lengh.minimap2.stderr
-#         echo -n $chr_id,$read_lengh,minimap2,align,
-#         python $eval_script_dir/get_accuracy.py --truth $outroot/$chr_id/$read_lengh.sam --predicted_sam $outroot/$chr_id/$read_lengh.minimap2.sam --time_mem $outroot/$chr_id/$read_lengh.minimap2.stderr
+#         # # minimap2 stats
+#         # /usr/bin/time -l minimap2 -t 1 --eqx -ax sr $refs/$chr_id.fa $outroot/$chr_id/$read_lengh.L.fq $outroot/$chr_id/$read_lengh.R.fq 1> $outroot/$chr_id/$read_lengh.minimap2.sam 2>  $outroot/$chr_id/$read_lengh.minimap2.stderr
+#         # # /usr/bin/time -l minimap2 -t 1 --eqx -ax map-hifi $refs/$chr_id.fa $outroot/$chr_id/$read_lengh.L.fq $outroot/$chr_id/$read_lengh.R.fq 1> $outroot/$chr_id/$read_lengh.minimap2.sam 2>  $outroot/$chr_id/$read_lengh.minimap2.stderr
+#         # echo -n $chr_id,$read_lengh,minimap2,align,
+#         # python $eval_script_dir/get_accuracy.py --truth $outroot/$chr_id/$read_lengh.sam --predicted_sam $outroot/$chr_id/$read_lengh.minimap2.sam --time_mem $outroot/$chr_id/$read_lengh.minimap2.stderr
+
+#         /usr/bin/time -l $strobealign_dev_dir/./strobealign -t 1 -r $read_lengh -o $outroot/$chr_id/$read_lengh.strobealign.sam $refs/$chr_id.fa $outroot/$chr_id/$read_lengh.L.fq $outroot/$chr_id/$read_lengh.R.fq &>  $outroot/$chr_id/$read_lengh.strobealign.stderr
+#         echo -n $chr_id,$read_lengh,strobealign,align,
+#         python $eval_script_dir/get_accuracy.py --truth $outroot/$chr_id/$read_lengh.sam --predicted_sam $outroot/$chr_id/$read_lengh.strobealign.sam --time_mem $outroot/$chr_id/$read_lengh.strobealign.stderr
+
 
 #         # /usr/bin/time -l $strobealign_dev_dir/./strobealign -t 1 -k 22 -l 0 -u 10 -o $outroot/$chr_id/$read_lengh.strobealign.sam $refs/$chr_id.fa $outroot/$chr_id/$read_lengh.L.fq $outroot/$chr_id/$read_lengh.R.fq &>  $outroot/$chr_id/$read_lengh.strobealign.stderr
 #         # echo -n $chr_id,$read_lengh,strobealign,align,8,22,0,10,
 #         # python $eval_script_dir/get_accuracy.py --truth $outroot/$chr_id/$read_lengh.sam --predicted_sam $outroot/$chr_id/$read_lengh.strobealign.sam --time_mem $outroot/$chr_id/$read_lengh.strobealign.stderr
 
-#         for bc in 8 #16 # 32
-#         do
-#             for k in 15 17 20 #19 20 #18 19 20 #18 19 20 21
-#             do
-#                 for l in -2
-#                 do
-#                     for u in 3 7 #3 5 #9 #5 7 9
-#                     do
+#         # for bc in 8 #16 # 32
+#         # do
+#         #     for k in 15 17 20 #19 20 #18 19 20 #18 19 20 21
+#         #     do
+#         #         for l in -2
+#         #         do
+#         #             for u in 3 7 #3 5 #9 #5 7 9
+#         #             do
 
-#                         /usr/bin/time -l $strobealign_dev_dir/./strobealign -t 1 -c $bc -k $k -l $l -u $u -o $outroot/$chr_id/$read_lengh.strobealign.sam $refs/$chr_id.fa $outroot/$chr_id/$read_lengh.L.fq $outroot/$chr_id/$read_lengh.R.fq &>  $outroot/$chr_id/$read_lengh.strobealign.stderr
-#                         echo -n $chr_id,$read_lengh,strobealign,align,$bc,$k,$l,$u,
-#                         python $eval_script_dir/get_accuracy.py --truth $outroot/$chr_id/$read_lengh.sam --predicted_sam $outroot/$chr_id/$read_lengh.strobealign.sam --time_mem $outroot/$chr_id/$read_lengh.strobealign.stderr
+#         #                 /usr/bin/time -l $strobealign_dev_dir/./strobealign -t 1 -c $bc -k $k -l $l -u $u -o $outroot/$chr_id/$read_lengh.strobealign.sam $refs/$chr_id.fa $outroot/$chr_id/$read_lengh.L.fq $outroot/$chr_id/$read_lengh.R.fq &>  $outroot/$chr_id/$read_lengh.strobealign.stderr
+#         #                 echo -n $chr_id,$read_lengh,strobealign,align,$bc,$k,$l,$u,
+#         #                 python $eval_script_dir/get_accuracy.py --truth $outroot/$chr_id/$read_lengh.sam --predicted_sam $outroot/$chr_id/$read_lengh.strobealign.sam --time_mem $outroot/$chr_id/$read_lengh.strobealign.stderr
 
-#                         # Mapping stats
-#                         # /usr/bin/time -l $strobealign_dev_dir/./strobealign -t 1 -k $k -x -o $outroot/$chr_id/$read_lengh.strobealign.paf $refs/$chr_id.fa $outroot/$chr_id/$read_lengh.L.fq $outroot/$chr_id/$read_lengh.R.fq &>  $outroot/$chr_id/$read_lengh.strobealign.stderr
-#                         # echo -n $chr_id,$read_lengh,strobealign_k20,map,
-#                         # python $eval_script_dir/get_accuracy.py --truth $outroot/$chr_id/$read_lengh.sam --predicted_paf $outroot/$chr_id/$read_lengh.strobealign.paf --time_mem $outroot/$chr_id/$read_lengh.strobealign.stderr
-#                     done
-#                 done
-#             done
-#             echo
-#         done
-#         echo "NEW CHR"
-#         echo
+#         #                 # Mapping stats
+#         #                 # /usr/bin/time -l $strobealign_dev_dir/./strobealign -t 1 -k $k -x -o $outroot/$chr_id/$read_lengh.strobealign.paf $refs/$chr_id.fa $outroot/$chr_id/$read_lengh.L.fq $outroot/$chr_id/$read_lengh.R.fq &>  $outroot/$chr_id/$read_lengh.strobealign.stderr
+#         #                 # echo -n $chr_id,$read_lengh,strobealign_k20,map,
+#         #                 # python $eval_script_dir/get_accuracy.py --truth $outroot/$chr_id/$read_lengh.sam --predicted_paf $outroot/$chr_id/$read_lengh.strobealign.paf --time_mem $outroot/$chr_id/$read_lengh.strobealign.stderr
+#         #             done
+#         #         done
+#         #     done
+#         #     echo
+#         # done
+#         # echo "NEW CHR"
+#         # echo
 #     done
 # done
 
 
-for read_lengh in 100 #150 200 250 300 
+for read_lengh in 100 150 200 250 300 
 do 
-    for chr_id in hg38_chr18 # hg38_chr1 hg38_chr15 hg38_chr18 hg38_chr21 hg38_chrX # hg38_chr1_2 hg38_chr6_15_18_X_Y  #  # hg38_chr1_2 hg38_chr6_15_18_X_Y  #
+    for chr_id in hg38_chr18 hg38_chrX hg38_chr21 hg38_chr1 hg38_chr15 # hg38_chr1_2 hg38_chr6_15_18_X_Y  #  # hg38_chr1_2 hg38_chr6_15_18_X_Y  #
     do
 
         # minimap2 stats
@@ -115,12 +122,12 @@ do
         # echo -n $chr_id,$read_lengh,minimap2,align,
         # python $eval_script_dir/get_accuracy.py --truth $outroot/$chr_id/$read_lengh.sam --predicted_sam $outroot/$chr_id/$read_lengh.minimap2.sam --time_mem $outroot/$chr_id/$read_lengh.minimap2.stderr
 
-        # /usr/bin/time -l $strobealign_dev_dir/./strobealign -t 1 -r $read_lengh -o $outroot/$chr_id/$read_lengh.strobealign.sam $refs/$chr_id.fa $outroot/$chr_id/$read_lengh.L.fq $outroot/$chr_id/$read_lengh.R.fq # &>  $outroot/$chr_id/$read_lengh.strobealign.stderr
+        /usr/bin/time   -l $strobealign_dev_dir/./strobealign -t 1 -r $read_lengh -o $outroot/$chr_id/$read_lengh.strobealign.sam $refs/$chr_id.fa $outroot/$chr_id/$read_lengh.L.fq $outroot/$chr_id/$read_lengh.R.fq &>  $outroot/$chr_id/$read_lengh.strobealign.stderr
         echo -n $chr_id,$read_lengh,strobealign,align,
         python $eval_script_dir/get_accuracy.py --truth $outroot/$chr_id/$read_lengh.sam --predicted_sam $outroot/$chr_id/$read_lengh.strobealign.sam --time_mem $outroot/$chr_id/$read_lengh.strobealign.stderr
-        python $eval_script_dir/get_erroneous.py --truth $outroot/$chr_id/$read_lengh.sam --predicted_sam_method1 $outroot/$chr_id/$read_lengh.minimap2.sam \
-                                                 --predicted_sam_method2 $outroot/$chr_id/$read_lengh.strobealign.sam --fq1 $outroot/$chr_id/$read_lengh.L.fq --fq2 $outroot/$chr_id/$read_lengh.R.fq --om $outroot/$chr_id/$read_lengh.misaligned \
-                                                 --ou $outroot/$chr_id/$read_lengh.misaligned
+        # python $eval_script_dir/get_erroneous.py --truth $outroot/$chr_id/$read_lengh.sam --predicted_sam_method1 $outroot/$chr_id/$read_lengh.minimap2.sam \
+        #                                          --predicted_sam_method2 $outroot/$chr_id/$read_lengh.strobealign.sam --fq1 $outroot/$chr_id/$read_lengh.L.fq --fq2 $outroot/$chr_id/$read_lengh.R.fq --om $outroot/$chr_id/$read_lengh.misaligned \
+        #                                          --ou $outroot/$chr_id/$read_lengh.misaligned
 
         # /usr/bin/time -l $strobealign_dev_dir/./strobealign -t 1 -R 0 -r $read_lengh -o $outroot/$chr_id/$read_lengh.strobealign.sam $refs/$chr_id.fa $outroot/$chr_id/$read_lengh.L.fq $outroot/$chr_id/$read_lengh.R.fq &>  $outroot/$chr_id/$read_lengh.strobealign.stderr
         # echo -n $chr_id,$read_lengh,strobealign,NO_RESC,
@@ -136,5 +143,26 @@ do
     done
     echo
 done
+
+
+###### FOR BUGFIXING #########
+
+# for read_lengh in 100 #150 200 250 300 
+# do 
+#     for chr_id in hg38_chr18 # hg38_chr1 hg38_chr15 hg38_chr18 hg38_chr21 hg38_chrX # hg38_chr1_2 hg38_chr6_15_18_X_Y  #  # hg38_chr1_2 hg38_chr6_15_18_X_Y  #
+#     do
+
+#         # minimap2 stats
+#         /usr/bin/time -l minimap2 -t 1 --eqx -ax sr $refs/$chr_id.fa $outroot/$chr_id/$read_lengh.misaligned_1.fq $outroot/$chr_id/$read_lengh.misaligned_2.fq 1> $outroot/$chr_id/$read_lengh.minimap2.sam 2>  $outroot/$chr_id/$read_lengh.minimap2.stderr
+#         echo -n $chr_id,$read_lengh,minimap2,align,
+#         python $eval_script_dir/get_accuracy.py --truth $outroot/$chr_id/$read_lengh.sam --predicted_sam $outroot/$chr_id/$read_lengh.minimap2.sam --time_mem $outroot/$chr_id/$read_lengh.minimap2.stderr
+
+#         /usr/bin/time -l $strobealign_dev_dir/./strobealign -t 1 -r $read_lengh -o $outroot/$chr_id/$read_lengh.strobealign.sam $refs/$chr_id.fa $outroot/$chr_id/$read_lengh.misaligned_1.fq $outroot/$chr_id/$read_lengh.misaligned_2.fq &>  $outroot/$chr_id/$read_lengh.strobealign.stderr
+#         echo -n $chr_id,$read_lengh,strobealign,align,
+#         python $eval_script_dir/get_accuracy.py --truth $outroot/$chr_id/$read_lengh.sam --predicted_sam $outroot/$chr_id/$read_lengh.strobealign.sam --time_mem $outroot/$chr_id/$read_lengh.strobealign.stderr
+
+#     done
+#     echo
+# done
 
 
