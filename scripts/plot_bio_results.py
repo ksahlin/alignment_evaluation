f23e95b0f5b1d989aa08d93e1db142394c50d4db
Kristoffer Sahlin <krsahlin@gmail.com>
2022-01-10 09:18:25 +0100

    +

--- scripts/plot_bio_results.py
+++ scripts/plot_bio_results.py
@@ -15,33 +15,26 @@ import pandas as pd
 from matplotlib import pyplot
 
 
-def plot_accuracy(input_csv, outfolder, palette, tools, linewidth = 2.5):
+def plot_agreement(input_csv, outfolder, palette, tools, linewidth = 2.5):
     matplotlib.rcParams.update({'font.size': 18})
     sns.set(font_scale=1.9)
     sns.set_style("whitegrid")
     indata = pd.read_csv(input_csv)
-    # "minimap2",'accelalign', "bowtie2", "strobealign_map", "minimap2_map", "accelalign_map"
-    # dashes = {"bwa_mem" : "", "bowtie2" : "",
-    #             "minimap2" : "", "minimap2_map" : (5,5),
-    #             "strobealign" : "", "strobealign_map" : (5,5),
-    #             "accelalign" : "", "accelalign_map" : (5,5)}
-    # print(indata)
-    g = sns.relplot(data=indata, x="read_length", y="accuracy", hue="tool", style="type", linewidth = linewidth, kind="line", #dashes = dashes,
-        col="dataset", hue_order = tools, # hue="datastructure", style="datastructure",
-        col_wrap=3, col_order=["SIM1", "SIM2", "SIM3"], palette=palette)
+    g = sns.relplot(data=indata, x="read_length", y="accuracy", hue="tool", linewidth = linewidth, kind="line", #dashes = dashes,
+        col="dataset", hue_order = tools,  palette=palette)
     # ax = sns.lineplot(data=indata, x="k", y="unique", hue="datastructure", style="chr", palette = sns.color_palette()[:7])
     # axes = g.axes
-    g.set_axis_labels("Read length", "Accuracy")
+    g.set_axis_labels("Read length", "Agreement with BWA and Bowtie2")
     # g.set_xticklabels([18,24,30,36])
     # ax.set_ylabel("% unique")
     # ax.set_xlabel("k")
     # axes.set_xticks([18,24,30,36] )
     # ax.set_ylim((75, 100))
-    g.set(ylim=(96, 99), xticks=[100,150,200,250,300])
+    g.set(ylim=(96, 99), xticks=[150,250])
     # g.set(ylim=(95, 100))
     # ax.set_xticks([18,24,30,36])
-    plt.savefig(os.path.join(outfolder, "accuracy_plot.eps"))
-    plt.savefig(os.path.join(outfolder, "accuracy_plot.pdf"))
+    plt.savefig(os.path.join(outfolder, "BIO_accuracy_plot.eps"))
+    plt.savefig(os.path.join(outfolder, "BIO_accuracy_plot.pdf"))
     plt.close()
 
 def plot_percentage_aligned(input_csv, outfolder, palette, tools, linewidth = 2.5):
@@ -51,20 +44,9 @@ def plot_percentage_aligned(input_csv, outfolder, palette, tools, linewidth = 2.
 
     indata = pd.read_csv(input_csv)
 
-    # dashes = { "strobemap" : "", 
-    #             "spaced_sparse": (5,5),
-    #             "spaced_dense": (5,5),
-    #              "minstrobes2" : (1,1),
-    #              "minstrobes3" : (1,1),
-    #              "randstrobes2" : (1,1),
-    #              "randstrobes3" : (1,1),
-    #              "hybridstrobes2" : (1,1),
-    #              "hybridstrobes3" : (1,1)}
-    # print(indata)
     g = sns.relplot(
-        data=indata, x="read_length", y="aligned", hue="tool", style="type", linewidth = linewidth,
-        col="dataset", kind="line",  hue_order = tools, #dashes = dashes, hue="datastructure", style="datastructure",
-        col_wrap=3, col_order=["SIM1", "SIM2", "SIM3"], palette=palette)
+        data=indata, x="read_length", y="percent_aligned", hue="tool", linewidth = linewidth,
+        col="dataset", kind="line",  hue_order = tools, palette=palette)
     # ax = sns.lineplot(data=indata, x="k", y="unique", hue="datastructure", style="chr", palette = sns.color_palette()[:7])
     # axes = g.axes
     g.set_axis_labels("Read length", "Percentage aligned")
@@ -73,76 +55,13 @@ def plot_percentage_aligned(input_csv, outfolder, palette, tools, linewidth = 2.
     # ax.set_xlabel("k")
     # axes.set_xticks([18,24,30,36] )
     # ax.set_ylim((75, 100))
-    g.set(ylim=(98, 100), xticks=[100,150,200,250,300])
+    g.set(ylim=(90, 100), xticks=[150,250])
     # g.set(ylim=(95, 100))
     # ax.set_xticks([18,24,30,36])
-    plt.savefig(os.path.join(outfolder, "percentage_aligned_plot.eps"))
-    plt.savefig(os.path.join(outfolder, "percentage_aligned_plot.pdf"))
+    plt.savefig(os.path.join(outfolder, "BIO_percentage_aligned_plot.eps"))
+    plt.savefig(os.path.join(outfolder, "BIO_percentage_aligned_plot.pdf"))
     plt.close()
 
-def plot_overaligned(input_csv, outfolder, palette, tools, linewidth = 2.5):
-    matplotlib.rcParams.update({'font.size': 18})
-    sns.set(font_scale=1.9)
-    sns.set_style("whitegrid")
-
-    indata = pd.read_csv(input_csv)
-
-    # dashes = { "strobemap" : "", 
-    #             "spaced_sparse": (5,5),
-    #             "spaced_dense": (5,5),
-    #              "minstrobes2" : (1,1),
-    #              "minstrobes3" : (1,1),
-    #              "randstrobes2" : (1,1),
-    #              "randstrobes3" : (1,1),
-    #              "hybridstrobes2" : (1,1),
-    #              "hybridstrobes3" : (1,1)}
-    # print(indata)
-    g = sns.relplot(
-        data=indata, x="read_length", y="overaligned", hue="tool", style="type", linewidth = linewidth,
-        col="dataset", kind="line", hue_order = tools,  #dashes = dashes, hue="datastructure", style="datastructure",
-        col_wrap=3, col_order=["SIM1", "SIM2", "SIM3"], palette=palette)
-    # ax = sns.lineplot(data=indata, x="k", y="unique", hue="datastructure", style="chr", palette = sns.color_palette()[:7])
-    # axes = g.axes
-    g.set_axis_labels("Read length", "Overaligned")
-    # g.set_xticklabels([18,24,30,36])
-    # ax.set_ylabel("% unique")
-    # ax.set_xlabel("k")
-    # axes.set_xticks([18,24,30,36] )
-    # ax.set_ylim((75, 100))
-    g.set( xticks=[100,150,200,250,300]) #ylim=(40, 100),
-    # g.set(ylim=(95, 100))
-    # ax.set_xticks([18,24,30,36])
-    plt.savefig(os.path.join(outfolder, "overaligned_plot.eps"))
-    plt.savefig(os.path.join(outfolder, "overaligned_plot.pdf"))
-    plt.close()
-
-
-def plot_memory_usage(input_csv, outfolder, palette, tools, linewidth = 2.5):
-    matplotlib.rcParams.update({'font.size': 18})
-    sns.set(font_scale=1.9)
-    # tool,dataset,read_length,time,memory
-    indata = pd.read_csv(input_csv)
-
-    sns.set_style("whitegrid")
-
-    g = sns.relplot(
-        data=indata, x="read_length", y="memory", hue="tool", style="type", linewidth = linewidth, 
-        col="dataset", kind="line", hue_order = tools,  #dashes = dashes, hue="datastructure", style="datastructure",
-        col_wrap=3, col_order=["SIM1", "SIM2", "SIM3"], palette=palette)
-    # ax = sns.lineplot(data=indata, x="k", y="unique", hue="datastructure", style="chr", palette = sns.color_palette()[:7])
-    # axes = g.axes
-    g.set_axis_labels("Read length", "Memory usage (Gb)")
-    # g.set_xticklabels([18,24,30,36])
-    # ax.set_ylabel("% unique")
-    # ax.set_xlabel("k")
-    # axes.set_xticks([18,24,30,36] )
-    # ax.set_ylim((75, 100))
-    g.set( xticks=[100,150,200,250,300]) #ylim=(40, 100),
-    # g.set(ylim=(95, 100))
-    # ax.set_xticks([18,24,30,36])
-    plt.savefig(os.path.join(outfolder, "memory_plot.eps"))
-    plt.savefig(os.path.join(outfolder, "memory_plot.pdf"))
-    plt.close()
 
 def plot_runtime(input_csv, outfolder, palette, tools, linewidth = 2.5):
     matplotlib.rcParams.update({'font.size': 18})
@@ -152,28 +71,22 @@ def plot_runtime(input_csv, outfolder, palette, tools, linewidth = 2.5):
 
     indata = pd.read_csv(input_csv)
     g = sns.relplot(
-        data=indata, x="read_length", y="time", hue="tool", style="type", linewidth = linewidth,
-        col="dataset", kind="line", hue_order = tools,  #dashes = dashes, hue="datastructure", style="datastructure",
-        col_wrap=3, col_order=["SIM1", "SIM2", "SIM3"], palette=palette)
+        data=indata, x="read_length", y="time", hue="tool", linewidth = linewidth,
+        col="dataset", kind="line", hue_order = tools,  palette=palette)
     # ax = sns.lineplot(data=indata, x="k", y="unique", hue="datastructure", style="chr", palette = sns.color_palette()[:7])
     # axes = g.axes
     g.set_axis_labels("Read length", "Time (sec)")
-    # g.set_xticklabels([18,24,30,36])
-    # ax.set_ylabel("% unique")
-    # ax.set_xlabel("k")
-    # axes.set_xticks([18,24,30,36] )
-    # ax.set_ylim((75, 100))
     g.set(yscale="log")
     g.set( yticks= [i for i in range(100,999,100)] + [i for i in range(1000,9999,1000)] + [i for i in range(10000,39999,10000)]) #, ylim=(0, 5200))
     # g.set_yticklabels( ["100"] + ["" for i in range(200,999,100)] + ["1000"] +  ["" for i in range(2000,9999,1000)] + ["10000"] +  [i for i in range(1000,2999,1000)]])
 
-    g.set( xticks=[100,150,200,250,300]) #ylim=(40, 100),
+    g.set( xticks=[150,250]) #ylim=(40, 100),
     # g.set( yticks=[0,1000,2000,4000,6000,12000,18000,24000]) #ylim=(40, 100),
 
-    # g.set(ylim=(95, 100))
+    # g.set(ylim=(95, 100))s
     # ax.set_xticks([18,24,30,36])
-    plt.savefig(os.path.join(outfolder, "time_plot.eps"))
-    plt.savefig(os.path.join(outfolder, "time_plot.pdf"))
+    # plt.savefig(os.path.join(outfolder, "BIOs_time_plot.eps"))
+    plt.savefig(os.path.join(outfolder, "BIOtime_plot.pdf"))
     plt.close()
 
 def add_column(infile):
@@ -215,20 +128,17 @@ def main(args):
     }
     tools =["minimap2", "bwa_mem", 'accelalign', "bowtie2",  "strobealign", "snap"] # "urmap",
 
-    accuracy_csv = add_column(args.accuracy_csv)
-    runtime_mem_csv = add_column(args.runtime_mem_csv)
+    # accuracy_csv = add_column(args.accuracy_csv)
+    # runtime_mem_csv = add_column(args.runtime_mem_csv)
 
-    plot_accuracy(accuracy_csv, args.outfolder, palette, tools, linewidth = 2.5)
-    plot_percentage_aligned(accuracy_csv, args.outfolder, palette, tools, linewidth = 2.5)
-    plot_overaligned(accuracy_csv, args.outfolder, palette, tools, linewidth = 2.5)
-    plot_runtime(runtime_mem_csv, args.outfolder, palette, tools, linewidth = 2.5)
-    plot_memory_usage(runtime_mem_csv, args.outfolder, palette, tools, linewidth = 2.5)
+    plot_percentage_aligned(args.csv, args.outfolder, palette, tools, linewidth = 2.5)
+    plot_runtime(args.csv, args.outfolder, palette, tools, linewidth = 2.5)
+    plot_agreement(args.csv, args.outfolder, palette, tools, linewidth = 2.5)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Calc identity", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('accuracy_csv', type=str, help='results file')
-    parser.add_argument('runtime_mem_csv', type=str, help='results file')
+    parser.add_argument('csv', type=str, help='results file')
     parser.add_argument('outfolder', type=str,  help='outfolder to plots.')
     args = parser.parse_args()
 
@@ -239,3 +149,32 @@ if __name__ == '__main__':
         sys.exit()
 
     main(args)
+
+
+# tool,dataset,read_length,time,memory,percent_aligned,agreement
+# minimap2,BIO150,150,1591.1380000000001,11.431484,0.98765,7388427
+# bwa_mem,BIO150,150,4676.0,5.468372,0.99362,0
+# strobealign,BIO150,150,799.4620000000001,35.75272,0.9902,7409251
+# accelalign,BIO150,150,738.45,19.305676,0.96928,7233470
+# bowtie2,BIO150,150,7930.0,3.402236,0.99374,0
+# snap,BIO150,150,1057.06,33.587392,0.97539,7384415
+# urmap,BIO150,150,742.76,29.426892,0.9743,7400486
+# minimap2,BIO250,250,2517.722,11.372176,0.99268,7431920
+# bwa_mem,BIO250,250,7861.0,5.415552,0.99713,0
+# strobealign,BIO250,250,970.3819999999998,35.917084,0.99258,7427035
+# accelalign,BIO250,250,1077.33,19.30904,0.98596,7303527
+# bowtie2,BIO250,250,19368.0,3.419988,0.99464,0
+# snap,BIO250,250,2807.45,33.586308,0.93357,7166512
+# urmap,BIO250,250,1422.84,29.427496,0.95306,7306319
+
+
+
+
+
+# strobealign_map,BIO150,150,487.591,35.752552,
+# minimap2_map,BIO150,150,1212.958,11.339444
+# accelalign_map,BIO150,150,747.45,19.305656
+
+# strobealign_map,BIO250,250,476.06199999999995,35.752712
+# minimap2_map,BIO250,250,1636.795,11.296968
+# accelalign_map,BIO250,250,1072.99,19.308
